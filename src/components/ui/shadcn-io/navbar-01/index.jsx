9f64f44202850a4afbe1277d9f510a9c8ce9c61a
dodat17 <clone14159265358@gmail.com>
2025-11-25 16:04:23 +0700

    fix(logo): fix blur state

--- src/components/ui/shadcn-io/navbar-01/index.jsx
+++ src/components/ui/shadcn-io/navbar-01/index.jsx
@@ -388,7 +388,7 @@ export const Navbar01 = React.forwardRef(
       <header
         ref={combinedRef}
         className={cn(
-          "fixed top-0 left-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 md:px-6 [&_*]:no-underline",
+          "fixed top-0 left-0 z-50 w-full border-b bg-background/95  supports-[backdrop-filter]:bg-background/60 px-4 md:px-6 [&_*]:no-underline",
           className
         )}
         {...props}
@@ -441,7 +441,7 @@ export const Navbar01 = React.forwardRef(
                   e.preventDefault();
                   handleNavigation(logoHref);
                 }}
-                className="flex items-center space-x-2 text-primary hover:text-primary/90 transition-colors cursor-pointer"
+                className="flex items-center space-x-2 text-primary hover:text-primary/90 focus:outline-none focus:text-primary active:text-primary transition-colors cursor-pointer"
               >
                 <div className="text-2xl">{logo}</div>
                 <span className="hidden font-bold text-xl sm:inline-block">
